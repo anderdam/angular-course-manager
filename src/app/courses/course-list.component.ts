3bce538c4ef462d10670e7503fd48915e2ff211b
Anderson R Damasceno <anderdam@gmail.com>
2021-07-07 11:55:20 -0300

    Primeiro commit curso angular bootcamp Santander

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,11 @@
+import { listLazyRoutes } from "@angular/compiler/src/aot/lazy_routes";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html'
+})
+
+export class CourseListComponent {
+
+}
